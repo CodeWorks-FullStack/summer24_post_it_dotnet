e78d5c47b1cb9d65a4d4ac13802121707ee18722
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-14 11:37:55 -0600

    Album class

---
+++ server/Models/Picture.cs
@@ -0,0 +1,11 @@
+namespace post_it_dotnet.Models;
+
+public class Picture
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string ImgUrl { get; set; }
+  public string CreatorId { get; set; }
+  public int AlbumId { get; set; }
+}
