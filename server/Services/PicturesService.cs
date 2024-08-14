4e2bf9d2791ed8400ab32f15345c72da69457b2b
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-14 12:09:45 -0600

    checking album archival status before picture submission

--- server/Services/PicturesService.cs
+++ server/Services/PicturesService.cs
@@ -8,14 +8,23 @@ namespace post_it_dotnet.Services;
 public class PicturesService
 {
   private readonly PicturesRepository _repository;
+  private readonly AlbumsService _albumsService;
 
-  public PicturesService(PicturesRepository repository)
+  public PicturesService(PicturesRepository repository, AlbumsService albumsService)
   {
     _repository = repository;
+    _albumsService = albumsService;
   }
 
   internal Picture CreatePicture(Picture pictureData)
   {
+    Album album = _albumsService.GetAlbumById(pictureData.AlbumId);
+
+    if (album.Archived) // if(album.Archived == true)
+    {
+      throw new Exception($"{album.Title} has been archived and is no longer accepting picture submissions");
+    }
+
     Picture picture = _repository.CreatePicture(pictureData);
     return picture;
   }
